c57a13c75c25bd7bfa9c052c9aead05999be3cc0
Roman Tkachenko <Roman-Turchenko2002@yandex.ru>
2022-05-17 18:57:23 +0300

    Update weather_api.dart

--- lib/api/weather_api.dart
+++ lib/api/weather_api.dart
@@ -6,11 +6,7 @@ import 'package:http/http.dart' as http;
 import 'package:forecast_app/utilities/constants.dart';
 
 class WeatherApi {
-  // Future<WeatherForecast> fetchWeatherForecast(
-  //     {String? city, bool? isCity}) async {
-  //   Location location = Location();//получаем локацию
-  //   await location.getCurrentLocation();
-  //   Map<String, String?> parameters;
+
    Future<WeatherForecast> fetchWeatherForecast(
      {String? city, }) async {
     var queryParametrs = {
@@ -18,26 +14,6 @@ class WeatherApi {
       'units': 'metric',
       'q': city,
     };
-
-  
-
-
-    // if (isCity == true) {//если мы передаем определенный город, то выполняем один запрос
-    //   var params = {
-    //     'appid': Constants.WEATHER_APP_ID,
-    //     'units': 'metric',
-    //     'q': city
-    //   };
-    //   parameters = params;
-    // } else {
-    //   var params = {//иначе передаем город по локации устройства
-    //     'appid': Constants.WEATHER_APP_ID,
-    //     'units': 'metric',
-    //     'lat': location.latitude.toString(),
-    //     'lon': location.longitude.toString(),
-    //   };
-    //   parameters = params;
-    // }
 
     var uri = Uri.https(Constants.WEATHER_BASE_URL_DOMAIN,
         Constants.WEATHER_FORECAST_PATH, queryParametrs);
